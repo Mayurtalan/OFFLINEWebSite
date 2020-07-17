661eb14bf7aeb25cff5ff8436feea1090bb1d58b
Mayurtalan <mayurtalan911@gmail.com>
2020-07-17 09:22:20 +0530

    Change in Login page remove code

--- src/main/java/com/page/LoginPage.java
+++ src/main/java/com/page/LoginPage.java
@@ -9,40 +9,6 @@ import org.testng.Assert;
 public class LoginPage {
 	
 	
-	@FindBy(id="email")
-	private WebElement username;
-
-	@FindBy(id="password")
-	private WebElement password;
-
-	@FindBy(xpath="//button")
-	private WebElement loginButton;
-
-	@FindBy(xpath="//a[text()='Register a new membership']")
-	private WebElement regLink;
-
-	public LoginPage(WebDriver driver) {
-		PageFactory.initElements(driver, this);
-	}
-
-	public void typeUserName(String uName) {
-		username.clear();
-		username.sendKeys(uName);
-	}
-	public void typePassword(String pass) {
-		password.clear();
-		password.sendKeys(pass);
-
-	}
-	public DashboardPage clickLoginButton(WebDriver driver) {
-		loginButton.click();
-		return new DashboardPage(driver);
-	}
-	
-	public RegistrationPage clickOnRegLink(WebDriver driver) {
-		regLink.click();
-		return new RegistrationPage(driver);
-	}
-	
+		
 	
 }
--- src/test/java/com/test/LoginTest.java
+++ src/test/java/com/test/LoginTest.java
@@ -12,31 +12,7 @@ import com.page.LoginPage;
 
 public class LoginTest {
 		
-		LoginPage lp;
-		WebDriver driver;
-		
-	 		@BeforeSuite
-	  		public void beforeMethod() {
-		  	System.setProperty("webdriver.chrome.driver", "C:/chromedriver81.exe");
-			driver = new ChromeDriver();
-			driver.get("file:///D:/jbk/Offline%20Website/index.html");
-			driver.manage().window().maximize();
-	 		}
-	 		
-	 		@Test(priority=2)
-	 		public void invalidLoginTest() {
-	 			lp.typeUserName("kian@gmail.com");
-	 			lp.typePassword("123456");
-	 			lp.clickLoginButton(driver);
-	 		}
-	 		
-	 		@Test(priority=3)
-	 		public void validLoginTest() {
-	 			lp.typeUserName("kiran@gmail.com");
-	 			lp.typePassword("123456");
-	 			lp.clickLoginButton(driver);
-	 		}
-	 		
+			 		
 	 		
 	 		
 }
